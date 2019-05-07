86cc7be4cf1a6cf541f6306965811cf183b2387f
baggiowangyu <baggiowangyu@gmail.com>
2019-05-07 21:26:39 +0800

    增加证书处理

--- libGxxGmCrypto/libGxxGmCryptoDemo/libGxxGmCryptoDemo.cpp
+++ libGxxGmCrypto/libGxxGmCryptoDemo/libGxxGmCryptoDemo.cpp
@@ -8,6 +8,12 @@
 
 #include "../libGxxGmCryptoEx/libGxxGmCryptoEx.h"
 
+#include "Poco/Crypto/PKCS12Container.h"
+#include "Poco/Crypto/RSAKey.h"
+#include "Poco/Crypto/Cipher.h"
+#include "Poco/Crypto/CipherFactory.h"
+#include "Poco/Crypto/CipherKey.h"
+
 #pragma comment(lib, "libGxxGmCryptoEx.lib")
 //#pragma comment(lib, "libcrypto.lib")
 //#pragma comment(lib, "libssl.lib")
@@ -33,7 +39,34 @@ int _tmain(int argc, _TCHAR* argv[])
 	crypto.Encrypt_v1(plain, cipher, key, key_len, "aes-128-cbc", iv, iv_len);
 
 	// 读取pfx文件，用私钥加密密钥
-	const char *pfx_file = "LEVAM.pfx";
+	int errCode = 0;
+	std::string errstr;
+	try
+	{
+		const char *pfx_file = "Gosuncn-levam.pfx";
+		Poco::Crypto::PKCS12Container pkcs12(pfx_file, "123456");
+		Poco::Crypto::EVPPKey evpkey = pkcs12.getKey();
+
+		Poco::Crypto::RSAKey *rsakey = new Poco::Crypto::RSAKey(evpkey);
+		Poco::Crypto::Cipher::Ptr pCipher = Poco::Crypto::CipherFactory::defaultFactory().createCipher(*rsakey, RSA_PADDING_PKCS1);
+
+		std::string protected_key = pCipher->encryptString((const char*)key, Poco::Crypto::Cipher::ENC_BASE64);
+	}
+	catch(Poco::Crypto::CryptoException &e)
+	{
+		errCode = e.code();
+		errstr = e.displayText();
+	}
+	catch(Poco::Crypto::OpenSSLException &e)
+	{
+		errCode = e.code();
+		errstr = e.displayText();
+	}
+	catch(Poco::Exception &e)
+	{
+		errCode = e.code();
+		errstr = e.displayText();
+	}
 
 	std::string new_plain;
 	crypto.Decrypt_v1(cipher, new_plain, key, key_len, "aes-128-cbc", iv, iv_len);
